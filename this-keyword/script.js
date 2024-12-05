92ab3e7782329348edab47d803e0125ae9827e72
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-05 10:50:36 +0100

    Add a function call

--- this-keyword/script.js
+++ this-keyword/script.js
@@ -1,24 +1,41 @@
-const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
+// const toggleButton = document.getElementById('theme-toggle');
+// const body = document.body;
 
-const themeToggler = {
-  isDarkMode: false,
+// const themeToggler = {
+//   isDarkMode: false,
   
-  toggleTheme: function () {
-    this.isDarkMode = !this.isDarkMode; 
+//   toggleTheme: function () {
+//     this.isDarkMode = !this.isDarkMode; 
 
-    if (this.isDarkMode) {
-      body.classList.remove('light-theme');
-      body.classList.add('dark-theme');
-      toggleButton.textContent = 'Switch to Light Mode';
-    } else {
-      body.classList.remove('dark-theme');
-      body.classList.add('light-theme');
-      toggleButton.textContent = 'Switch to Dark Mode';
-    }
-  },
-};
+//     if (this.isDarkMode) {
+//       body.classList.remove('light-theme');
+//       body.classList.add('dark-theme');
+//       toggleButton.textContent = 'Switch to Light Mode';
+//     } else {
+//       body.classList.remove('dark-theme');
+//       body.classList.add('light-theme');
+//       toggleButton.textContent = 'Switch to Dark Mode';
+//     }
+//   },
+// };
 
-toggleButton.addEventListener('click', function () {
-  themeToggler.toggleTheme();
-});
+// toggleButton.addEventListener('click', function () {
+//   themeToggler.toggleTheme();
+// });
+
+
+// let car = {
+//     brand: 'Honda',
+//     getBrand: function () {
+//         return this.brand;
+//     }
+// };
+
+// console.log(car.getBrand()); // Honda
+function addOne(num) {
+  return num + 1;
+}
+
+let count = 5;
+
+console.log(addOne(count));
