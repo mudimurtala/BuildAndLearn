d6008bccc7f830a6bcb8f08bfdc2a9b843cb7614
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-05 10:57:39 +0100

    Add a function call

--- this-keyword/script.js
+++ this-keyword/script.js
@@ -33,9 +33,6 @@
 
 // console.log(car.getBrand()); // Honda
 
-function addOne(num) {
-  return num + 1;
-}
 const arr = [1, 2, 3, 4, 5];
-const mappedArr = arr.map(addOne);
+const mappedArr = arr.map((num) => num + 1);
 console.log(mappedArr); // Outputs [2, 3, 4, 5, 6]
