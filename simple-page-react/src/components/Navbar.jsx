6ee77d1c6ea100dfa4d190a3c22c5931000baa5f
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-12 13:58:59 +0100

    Add files

---
+++ simple-page-react/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="public/images/react-logo.png" alt="React logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
