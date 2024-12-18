ccde382b4f4a0de318b04ceedc07436bb168453c
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-18 12:05:01 +0100

    Add files

--- practiceReact/App.jsx
+++ practiceReact/App.jsx
@@ -1,28 +1,32 @@
 import Contact from "./Contact"
+import mrWhiskerson from "/images/mr-whiskerson.png"
+import mrFluffykins from "/images/fluffykins.png"
+import mrFelix from "/images/felix.png"
+import mrPumpkin from "/images/pumpkin.png"
 
 export default function App() {
     return (
         <div className="contacts">
             <Contact
-                img="./images/mr-whiskerson.png"
+                img={mrWhiskerson}
                 name="Mr. Whiskerson"
                 phone="(212) 555-1234"
                 email="mr.whiskaz@catnap.meow"
             />
             <Contact
-                img="./images/fluffykins.png"
+                img={mrFluffykins}
                 name="Fluffykins"
                 phone="(212) 555-2345"
                 email="fluff@me.com"
             />
             <Contact
-                img="./images/felix.png"
+                img={mrFelix}
                 name="Felix"
                 phone="(212) 555-4567"
                 email="thecat@hotmail.com"
             />
             <Contact 
-                img="./images/pumpkin.png"
+                img={mrPumpkin}
                 name="Pumpkin"
                 phone="(0800) CAT KING"
                 email="pumpkin@scrimba.com"
