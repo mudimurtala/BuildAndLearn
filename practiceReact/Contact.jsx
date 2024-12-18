57fb14f7f75700cd619deef63cf5e655dd3ed562
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-18 02:41:35 +0100

    Add files

--- practiceReact/Contact.jsx
+++ practiceReact/Contact.jsx
@@ -2,20 +2,20 @@ export default function Contact() {
     return (
         <article className="contact-card">
             <img
-                src="./images/mr-whiskerson.png"
+                src="public/images/mr-whiskerson.png"
                 alt="Photo of Mr. Whiskerson"
             />
             <h3>Mr. Whiskerson</h3>
             <div className="info-group">
                 <img
-                    src="./images/phone-icon.png"
+                    src="/images/phone-icon.png"
                     alt="phone icon"
                 />
                 <p>(212) 555-1234</p>
             </div>
             <div className="info-group">
                 <img
-                    src="./images/mail-icon.png"
+                    src="/images/mail-icon.png"
                     alt="mail icon"
                 />
                 <p>mr.whiskaz@catnap.meow</p>
