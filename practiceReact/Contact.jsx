06d3e863d75b79cd1d6e99a35daaa94c2703e1ae
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-18 06:58:02 +0100

    Add files

--- practiceReact/Contact.jsx
+++ practiceReact/Contact.jsx
@@ -1,24 +1,24 @@
-export default function Contact({img, phone, name, email}) {
+export default function Contact(props) {
     return (
         <article className="contact-card">
             <img
-                src={img}
+                src={props.img}
                 alt="Photo of Mr. Whiskerson"
             />
-            <h3>{name}</h3>
+            <h3>{props.name}</h3>
             <div className="info-group">
                 <img
                     src="/images/phone-icon.png"
                     alt="phone icon"
                 />
-                <p>{phone}</p>
+                <p>{props.phone}</p>
             </div>
             <div className="info-group">
                 <img
                     src="/images/mail-icon.png"
                     alt="mail icon"
                 />
-                <p>{email}</p>
+                <p>{props.email}</p>
             </div>
         </article>
     )
