6b1f12e8526a69037cdbff68dcfb8d12fe00a144
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-18 02:29:06 +0100

    Add files

---
+++ practiceReact/App.jsx
@@ -0,0 +1,14 @@
+import Contact from "./Contact"
+
+function App() {
+    return (
+        <div className="contacts">
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+        </div>
+    )
+}
+
+export default App
