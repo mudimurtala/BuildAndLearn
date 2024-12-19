1a1b274c8eb0c93eea1e3926e89eb996584be659
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-19 08:42:00 +0100

    Add file

--- joke-react/Joke.jsx
+++ joke-react/Joke.jsx
@@ -1,9 +1,20 @@
-export default function Joke(prop) {
+export default function Joke(props) {
     return (
         <>
-            <p className="setup">{prop.Setup}</p>
-            <p className="punchline">{prop.Punchline}</p>
+            <p className="setup">Setup: {props.setup}</p>
+            <p className="punchline">Punchline: {props.punchline}</p>
             <hr />
         </>
     )
 }
+
+
+// export default function Joke(props) {
+//     return (
+//         <>
+//             {props.setup && <p className="setup">Setup: {props.setup}</p>}
+//             <p className="punchline">Punchline: {props.punchline}</p>
+//             <hr />
+//         </>
+//     )
+// }
