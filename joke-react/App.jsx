f7db67351e2bc6c5f6028847813c576da9bc5f7c
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-19 08:41:44 +0100

    Add file

--- joke-react/App.jsx
+++ joke-react/App.jsx
@@ -16,32 +16,39 @@
  * If you don't pass in a "question" prop, how might you make it only 
  * show the punchline?
  */
+
+
+
+
+// import Joke from "./Joke"
+// import jokesData from "./jokesData"
+
+// export default function App() {
+//     const jokeElements = jokesData.map((joke) => {
+//         return <Joke 
+//                     setup={joke.setup}
+//                     punchline={joke.punchline}
+//                 />
+//     })
+//     return (
+//         <main>
+//             {jokeElements}
+//         </main>
+//     )
+// }
+
+
+
 import Joke from "./Joke"
+import jokesData from "./jokesData"
 
 export default function App() {
+    const jokeElements = jokesData.map((joke) => {
+        return <Joke setup={joke.setup} punchline={joke.punchline} />
+    })
     return (
         <main>
-            <Joke 
-                Setup="I got my daughter a fridge for her birthday."
-                Punchline="I can't wait to see her face light up when she opens it."
-                
-            />
-            <Joke 
-                Setup="How did the hacker escape the police?"
-                Punchline="He just ransomware!"
-            />
-            <Joke 
-                Setup="Why don't pirates travel on mountain roads?"
-                Punchline="Scurvy."
-            />
-            <Joke 
-                Setup="Why do bees stay in the hive in the winter?"
-                Punchline="Swarm."
-            />
-            <Joke 
-                Setup="What's the best thing about Switzerland?"
-                Punchline="I don't know, but the flag is a big plus!"
-            />
+            {jokeElements}
         </main>
     )
 }
