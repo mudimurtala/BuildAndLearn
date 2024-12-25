12810bc9a804e89e930a9850029006e4730217f2
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-25 12:32:36 +0100

    Add files

--- re-travel-journal/App.jsx
+++ re-travel-journal/App.jsx
@@ -1,35 +1,37 @@
-import Header from "/components/Header";
-import Entry from "/components/Entry";
+import Header from "./components/Header"
+import Entry from "./components/Entry"
+import data from "./data"
 
 /**
- * Challenge: pass props to the Entry component to display
- * its data. See the `data.md` file for each prop name and its
- * value.
- * 
- * Then on the Entry component, receive and display the values
- * for those props. In the end, the page should look the same
- * as it does now, but without all the hard-coded data in the
- * component
+ * Challenge:
+ * - import the array of data from data.js
+ * - map over the array to create an <Entry /> component
+ *   for every item in the data array.
+ * - display the array of Entry components in place of the current
+ *   hard-coded <Entry /> instance.
  */
 
 export default function App() {
+    const dataElements = data.map((item) => {
+        return (
+            <Entry 
+                img={{
+                    src: item.img.src,
+                    alt: item.img.alt
+                }}
+                title={item.title}
+                country={item.country}
+                googleMapsLink={item.googleMapsLink}
+                dates={item.dates}
+                text={item.text}
+            />
+        )
+    })
     return (
         <>
-            < Header />
+            <Header />
             <main className="container">
-                < Entry 
-                    img={
-                        {
-                            src: "https://scrimba.com/links/travel-journal-japan-image-url",
-                            alt: "Mount Fuji"
-                        }
-                    }
-                    title="Mount Fuji"
-                    country="Japan"
-                    googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-                    dates="12 Jan, 2021 - 24 Jan, 2021"
-                    text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-                />
+                {dataElements}
             </main>
         </>
     )
