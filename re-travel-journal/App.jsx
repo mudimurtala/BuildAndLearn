fec6c9ff95afb95b9ee1314c8d778bdafa4ad29a
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-25 14:20:15 +0100

    Add files

--- re-travel-journal/App.jsx
+++ re-travel-journal/App.jsx
@@ -16,17 +16,18 @@ export default function App() {
         return (
             <Entry 
                 key={item.id}
-                img={{
-                    src: item.img.src,
-                    alt: item.img.alt
-                }}
-                // Or this bolow
-                // img={item.img}
-                title={item.title}
-                country={item.country}
-                googleMapsLink={item.googleMapsLink}
-                dates={item.dates}
-                text={item.text}
+                item={item}
+                // img={{
+                //     src: item.img.src,
+                //     alt: item.img.alt
+                // }}
+                // // Or this bolow
+                // // img={item.img}
+                // title={item.title}
+                // country={item.country}
+                // googleMapsLink={item.googleMapsLink}
+                // dates={item.dates}
+                // text={item.text}
             />
         )
     })
