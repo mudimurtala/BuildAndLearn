dfba6a8b1440a4bb22ba17f33fa60efbf0211f3f
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-26 17:41:34 +0100

    Add files

--- chef-claude/App.jsx
+++ chef-claude/App.jsx
@@ -1,7 +1,13 @@
+import Header from "./Header"
+
+/**
+ * Challenge: Build the Header component in a separate file
+ * and render it here in the App component
+ */
 
 
 export default function App() {
     return (
-        <h1>Hello World!</h1>
+        <Header />
     )
 }
