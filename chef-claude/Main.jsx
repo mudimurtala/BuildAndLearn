821226f973568641fd263b447ad5964eed7559ec
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-31 15:34:06 +0100

    Add files

--- chef-claude/Main.jsx
+++ chef-claude/Main.jsx
@@ -6,13 +6,15 @@ export default function Main() {
             {ingredient}
         </li>
     )
+
+    function onSubmit() {
+        console.log("Form submitted!")
+    }
     
     /**
-     * Review Challenge:
-     * Map over the list of ingredients and render them as list items
-     * 
-     * Note: We're doing things a weird way here. Don't worry,
-     * we're building up to learning the right way 🙂
+     * Challenge:
+     * Add an `onSubmit` event listener on the form. Have the function
+     * simply console.log("Form submitted!") for now
      */
 
     return (
@@ -23,7 +25,9 @@ export default function Main() {
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
                 />
-                <button>Add ingredient</button>
+                <button onSubmit={onSubmit}>
+                    Add ingredient
+                </button>
             </form>
             <ul>
                 {ingredientsListItems}
