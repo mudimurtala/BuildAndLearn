73dd53fc914781023e1185d742cfbcb163b81161
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-31 12:04:42 +0100

    Add files

--- chef-claude/index.jsx
+++ chef-claude/index.jsx
@@ -1,13 +1,16 @@
 import ReactDom from "react-dom/client";
 
 function App() {
+    function onClickFunc() {
+        console.log("It is Clicked")
+    }
     return (
         <main className="container">
             <img 
                 src="https://picsum.photos/640/360" 
                 alt="Placeholder image for Picsum" 
             />
-            <button onClick={() => console.log("Clicked")}>Click me</button>
+            <button onClick={onClickFunc}>Click me</button>
         </main>
     )
 }
