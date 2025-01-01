e706a73bd5f762d2667821d4e5275989dc541ea8
mudimurtala <ibnmuhyideen95@gmail.com>
2025-01-01 09:47:53 +0100

    Add files

---
+++ simple-page-react/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <main>
+            <h1 className="title">Is state important to know?</h1>
+            <button className="value">Yes</button>
+        </main>
+    )
+}
