12cf29d30a4222d5afd7b5ae1727bb6c7ebad591
mudimurtala <ibnmuhyideen95@gmail.com>
2025-01-01 14:19:34 +0100

    Add files

--- simple-page-react/App.jsx
+++ simple-page-react/App.jsx
@@ -10,9 +10,9 @@ export default function App() {
         <main className="container">
             <h1>How many times will Bob say "state" in this section?</h1>
             <div className="counter">
-                <button className="minus"></button>
+                <button className="minus" aria-label="Decrease count"></button>
                 <h2 className="count"></h2>
-                <button className="plus"></button>
+                <button className="plus" aria-label="Increase count"></button>
             </div>
         </main>
     )
