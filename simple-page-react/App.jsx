d7835af8fc18714a010fcdb06f27fc155f33e9fa
mudimurtala <ibnmuhyideen95@gmail.com>
2025-01-01 11:57:20 +0100

    Add files

--- simple-page-react/App.jsx
+++ simple-page-react/App.jsx
@@ -1,8 +1,18 @@
+import React from "react"
+
 export default function App() {
+
+/**
+     * Challenge: Replace our hard-coded "Yes" on the page with 
+     * some state initiated with React.useState()
+     */
+
+    const result = React.useState("Yes Always");
+    console.log(result)
     return (
         <main>
             <h1 className="title">Is state important to know?</h1>
-            <button className="value">Yes</button>
+            <button className="value">{result[0]}</button>
         </main>
     )
 }
