43f581466e009bb53c3a53157b24fe15f8e3723e
mudimurtala <ibnmuhyideen95@gmail.com>
2025-01-01 12:55:15 +0100

    Add files

--- simple-page-react/App.jsx
+++ simple-page-react/App.jsx
@@ -1,10 +1,7 @@
 import React from "react"
 
 export default function App() {
-    const [isImportant, setIsImportant] = React.useState("Yes");
-    function handleClick() {
-        setIsImportant("So Yes")
-    }
+    
     return (
         <main>
             <h1 className="title">Is state important to know?</h1>
