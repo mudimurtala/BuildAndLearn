6b1e510511f9fad5953783a7755111e57ed77e65
mudimurtala <ibnmuhyideen95@gmail.com>
2025-01-01 16:57:54 +0100

    Add files

--- simple-page-react/App.jsx
+++ simple-page-react/App.jsx
@@ -3,11 +3,8 @@ import React from "react"
 export default function App() {
     const [count, setCount] = React.useState(0)
     function add() {
-        setCount(function(prevCount) {
-            return prevCount + 1
-        })
+        setCount(prevCount => prevCount + 1)
     }
-
     function subtract() {
         setCount(() => count - 1)
     }
