59483200db8433d8579b7dc1f35a384ba37fd7ea
mudimurtala <ibnmuhyideen95@gmail.com>
2025-01-01 15:03:19 +0100

    Add files

--- simple-page-react/App.jsx
+++ simple-page-react/App.jsx
@@ -2,22 +2,20 @@ import React from "react"
 
 export default function App() {
     const [isResult, setIsResult] = React.useState(0)
-    function handleClick() {
-        setIsResult(prevResult => prevResult + 1);
+    function add() {
+        setIsResult(isResult + 1)
     }
 
-    /**
-     * Challenge: 
-     * Create state to track our count value (initial value is 0)
-     * Don't forget to replace the hard-coded "0" with your new state
-     */
+    function subtract() {
+        setIsResult(isResult - 1)
+    }
     return (
         <main className="container">
             <h1>How many times will Bob say "state" in this section?</h1>
             <div className="counter">
-                <button onClick={() => setIsResult(isResult - 1)} className="minus" aria-label="Decrease count">-</button>
+                <button onClick={subtract} className="minus" aria-label="Decrease count">-</button>
                 <h2 className="count">{isResult}</h2>
-                <button onClick={handleClick} className="plus" aria-label="Increase count">+</button>
+                <button onClick={add} className="plus" aria-label="Increase count">+</button>
             </div>
         </main>
     )
