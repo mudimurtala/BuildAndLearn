eb9d96343ebc3f17c222353a892cca588211201b
mudimurtala <ibnmuhyideen95@gmail.com>
2025-01-02 04:50:20 +0100

    Add files

--- simple-page-react/App.jsx
+++ simple-page-react/App.jsx
@@ -2,12 +2,13 @@ import React from "react"
 
 export default function App() {
     /**
-     * Challenge: Replace the if/else below with a ternary
-     * to determine the text that should display on the page
+     * Challenge: move our ternary directly inside of the JSX
+     * so the "Yes" and "No" are determined inside the <h1>
+     * 
+     * Hint: you will no longer need the `answer` variable
      */
-    const isGoingOut = false
-    
-    const answer = isGoingOut ? "Yes" : "No";
+    const isGoingOut = true
+    const answer = isGoingOut ? "Yes" : "No"
     
     return (
         <main>
