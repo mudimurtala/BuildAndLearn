486d539e8639e1d909f2a57bfc975341bd69e46f
mudimurtala <ibnmuhyideen95@gmail.com>
2025-01-02 04:27:59 +0100

    Add files

--- simple-page-react/App.jsx
+++ simple-page-react/App.jsx
@@ -1,24 +1,12 @@
 import React from "react"
 
 export default function App() {
-    const [count, setCount] = React.useState(0)
-
-    function add() {
-        setCount(prevCount => prevCount + 1)
-    }
-
-    function subtract() {
-        setCount(prevCount => prevCount - 1)
-    }
+    
 
     return (
-        <main className="container">
-            <h1>How many times will Bob say "state" in this section?</h1>
-            <div className="counter">
-                <button onClick={subtract} className="minus" aria-label="Decrease count">-</button>
-                <h2 className="count">{count}</h2>
-                <button onClick={add} className="plus" aria-label="Increase count">+</button>
-            </div>
+        <main>
+            <h1 className="title">Do I feel like going out tonight?</h1>
+            <button className="value"></button>
         </main>
     )
 }
