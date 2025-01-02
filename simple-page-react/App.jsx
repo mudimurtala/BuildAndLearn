448a34e377ec4b292746b1ac7170f73e20537233
mudimurtala <ibnmuhyideen95@gmail.com>
2025-01-02 04:46:56 +0100

    Add files

--- simple-page-react/App.jsx
+++ simple-page-react/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
      * Challenge: Replace the if/else below with a ternary
      * to determine the text that should display on the page
      */
-    const isGoingOut = true
+    const isGoingOut = false
     
     let answer = isGoingOut ? "Yes" : "No";
     
