0f32a49d961307adb6095de59c5760cf1b41bfc1
mudimurtala <ibnmuhyideen95@gmail.com>
2025-01-02 04:37:11 +0100

    Add files

--- simple-page-react/App.jsx
+++ simple-page-react/App.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 
 export default function App() {
-    
+    const isGoingOut = true;
+    let answer;
 
+    if (isGoingOut === true) {
+        answer = "Yes";
+    } else {
+        answer = "No";
+    }
     return (
         <main>
             <h1 className="title">Do I feel like going out tonight?</h1>
