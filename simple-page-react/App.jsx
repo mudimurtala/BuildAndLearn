a35a56f6bba7a107c086c7caa21262620b3436df
mudimurtala <ibnmuhyideen95@gmail.com>
2025-01-02 04:44:29 +0100

    Add files

--- simple-page-react/App.jsx
+++ simple-page-react/App.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 
 export default function App() {
-    const isGoingOut = true;
-    let answer;
-
-    if (isGoingOut === true) {
-        answer = "Yes";
-    } else {
-        answer = "No";
-    }
+    /**
+     * Challenge: Replace the if/else below with a ternary
+     * to determine the text that should display on the page
+     */
+    const isGoingOut = true
+    
+    isGoingOut === true ? answer = "Yes" : "No";
+    
     return (
         <main>
             <h1 className="title">Do I feel like going out tonight?</h1>
