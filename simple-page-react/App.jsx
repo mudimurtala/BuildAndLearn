e27e6a6202e6a1f71c36ca59776b3a34035d1b4b
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-09 14:07:36 +0100

    Add

--- simple-page-react/App.jsx
+++ simple-page-react/App.jsx
@@ -1,28 +1,39 @@
+// import React from "react"
+
+// export default function App() {
+//     const [myFavoriteThings, setMyFavoriteThings] = React.useState([]);
+//     const allFavoriteThings = ["💦🌹", "😺", "💡🫖", "🔥🧤", "🟤🎁", 
+//     "🐴", "🍎🥧", "🚪🔔", "🛷🔔", "🥩🍝"]
+//     const thingsElements = myFavoriteThings.map(thing => <p key={thing}>{thing}</p>)
+
+//     function addFavoriteThing() {
+//         setMyFavoriteThings(prevFavThings => [...prevFavThings, allFavoriteThings[prevFavThings.length]])
+//     }
+    
+//     return (
+//         <main>
+//             <button onClick={addFavoriteThing}>Add item</button>
+//             <section aria-live="polite">
+//                 {thingsElements}
+//             </section>
+//         </main>
+//     )
+// }
+
+
+
 import React from "react"
 
 export default function App() {
-    /**
-     * Challenge: Convert the code below to use an array
-     * held in state instead of a local variable. Initialize 
-     * the state array as an empty array
-     * 
-     * Don't worry about fixing `addFavoriteThing` quite yet.
-     */
-    const [myFavoriteThings, setMyFavoriteThings] = React.useState([]);
-    const allFavoriteThings = ["💦🌹", "😺", "💡🫖", "🔥🧤", "🟤🎁", 
-    "🐴", "🍎🥧", "🚪🔔", "🛷🔔", "🥩🍝"]
-    const thingsElements = myFavoriteThings.map(thing => <p key={thing}>{thing}</p>)
 
-    function addFavoriteThing() {
-        // We'll work on this next, nothing to do here yet.
-        setMyFavoriteThings(prevFavThings => [...prevFavThings, allFavoriteThings[prevFavThings.length]])
-    }
-    
+    const [myFavoriteThings, setMyFavoriteThings] = React.useState([])
+    const allFavoriteThings = 
+
     return (
         <main>
-            <button onClick={addFavoriteThing}>Add item</button>
+            <button>Add item</button>
             <section aria-live="polite">
-                {thingsElements}
+
             </section>
         </main>
     )
