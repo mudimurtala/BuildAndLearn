60be0bef6f0b70af441a345df2979d8319efb304
mudimurtala <ibnmuhyideen95@gmail.com>
2025-06-13 09:18:32 +0100

    Add to files

---
+++ censusair/webapp/utils.py
@@ -0,0 +1,12 @@
+# webapp/utils.py
+
+import joblib
+import numpy as np
+
+def load_model(model_path="../models/occupancy_model.pkl"):
+    return joblib.load(model_path)
+
+def make_prediction(model, co2, temp, humidity):
+    input_data = np.array([[co2, temp, humidity]])
+    prediction = model.predict(input_data)
+    return int(round(prediction[0]))
