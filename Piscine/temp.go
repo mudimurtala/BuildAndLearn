d05c698d451dcdc075f42a820de2ef1cda12435b
mudimurtala <ibnmuhyideen95@gmail.com>
2025-10-26 07:24:57 +0100

    Add files

--- Piscine/temp.go
+++ Piscine/temp.go
@@ -1,5 +1,9 @@
 package main
 
+// Write a function that prints, in ascending order and on a single line: 
+// all unique combinations of three different digits so that, the first digit is lower than the second, and the second is lower than the third.
+// These combinations are separated by a comma and a space.
+
 import "github.com/01-edu/z01"
 
 func main() {
